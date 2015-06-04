a19451aee6ee586da6ee63fbace85a43e05ff6ce
zll <zll@coloc.cc>
2015-06-04 13:13:27 +0800

    完善一些样式

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,16 @@
 
 module ItemsHelper
-  def parse_content(content)
-    #    markdown(coderay(content))
-    #markdown(content)
+  def prettify_markup(markup)
+    case
+    when markup == "markdown"
+      "Markdown"
+    when markup == "rst"
+      "reStructuredText"
+    else
+      "unknown"
+    end
+  end
+  def all_markups
+    [["Markdown", "markdown"], ["reStructuredText", "rst"]]
   end
 end
